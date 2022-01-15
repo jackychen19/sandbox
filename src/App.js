7eacf897202dfa8dcedf99db60ce0ef2716b1204
Jacky Chen <jacky.chen0319@gmail.com>
2022-01-14 22:35:48 -0800

    Installed TailwindCSS

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className='p-12 max-w-sm mx-auto bg-white rounded-l shadow-lg flex items-center space-x-4'>
-      <p className='text-xl text-red'>Hello world!</p>
-      <p>Hello world!</p>
-      <p>Hello world!</p>
+    <div>
+      Start coding here!
     </div>
   );
 }
